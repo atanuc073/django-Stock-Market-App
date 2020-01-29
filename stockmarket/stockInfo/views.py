39514703487b808e0382df3e733bf413374c68df
Atanu Chowdhury <atanuc073@gmail.com>
2020-01-29 15:33:30 +0530

    add navbar

--- stockmarket/stockInfo/views.py
+++ stockmarket/stockInfo/views.py
@@ -3,4 +3,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
+    # api-key: pk_40d960e9e11f47419c47ed2a9de813c9
     return render(request,'home.html',{})
+
+def about(request):
+    return render(request,'about.html',{})
