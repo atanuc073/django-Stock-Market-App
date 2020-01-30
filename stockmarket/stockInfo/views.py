f9dd1f0fa7cfe356777f2e4b7f0e6c7aa952e36c
Atanu Chowdhury <atanuc073@gmail.com>
2020-01-30 20:31:36 +0530

    connect to the api

--- stockmarket/stockInfo/views.py
+++ stockmarket/stockInfo/views.py
@@ -3,8 +3,16 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
+    import requests
+    import json
+    api_request = requests.get("https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_40d960e9e11f47419c47ed2a9de813c9")
+
+    try:
+        api = json.loads(api_request.content)
+    except Exception as e:
+        api = "Error..."
     # api-key: pk_40d960e9e11f47419c47ed2a9de813c9
-    return render(request,'home.html',{})
+    return render(request,'home.html',{ 'api' : api })
 
 def about(request):
     return render(request,'about.html',{})
