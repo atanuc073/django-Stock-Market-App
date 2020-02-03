c611d73855b36ab174c2e80a26974801066d1687
Atanu Chowdhury <atanuc073@gmail.com>
2020-02-03 18:38:57 +0530

    made the stock information dynamic

--- stockmarket/stockInfo/views.py
+++ stockmarket/stockInfo/views.py
@@ -5,14 +5,22 @@ from django.shortcuts import render
 def home(request):
     import requests
     import json
-    api_request = requests.get("https://cloud.iexapis.com/stable/stock/aapl/quote?token=pk_40d960e9e11f47419c47ed2a9de813c9")
 
-    try:
-        api = json.loads(api_request.content)
-    except Exception as e:
-        api = "Error..."
-    # api-key: pk_40d960e9e11f47419c47ed2a9de813c9
-    return render(request,'home.html',{ 'api' : api })
+    if request.method == 'POST':
+        ticker=request.POST['ticker']
+        api_request = requests.get("https://cloud.iexapis.com/stable/stock/"+ ticker +"/quote?token=pk_40d960e9e11f47419c47ed2a9de813c9")
+
+        try:
+            api = json.loads(api_request.content)
+        except Exception as e:
+            api = "Error..."
+        # api-key: pk_40d960e9e11f47419c47ed2a9de813c9
+
+        return render(request,'home.html',{ 'api' : api })
+        
+    else:
+        return render(request,'home.html',{ 'ticker' : "Enter a ticker symbol above..." })
+
 
 def about(request):
     return render(request,'about.html',{})
